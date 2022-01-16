32b713875df914428c8f98ab9f380b85fdea7ae7
madinatulmusyarifah <97837807+madinatulmusyarifah@users.noreply.github.com>
2022-01-16 23:41:24 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //portofolio.
+    cout<< "Hello World \n" <<endl;
+    cout<< "\n Selamat Datang di dasar pemrograman" <<endl;
+    cout<< "Perkenalkan Nama Saya \n" <<endl;
+    cout<< "\n Biodata Diri \n" <<endl;
+    cout<< "Nama      : Madinatul Musyarifah \n" <<endl;
+    cout<< "NIM       : A11.2021.13938 \n" <<endl;
+    cout<< "Hobi      : Olahraga \n" <<endl;
+    cout<< "Instagram : madinna.my \n"<<endl;
+    cout<< "\n sekian perkenalan dari saya Terima Kasih \n"<<endl;
+
+
+    return 0;
+}
